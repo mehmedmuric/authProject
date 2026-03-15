21369b8cb6364d90826c77a2d1d2014cbabc048c
Mehmed Muric <mehmedmuric22@gmail.com>
2026-03-15 04:30:55 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// console.log('Hello, World!');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "Hello from server!"});
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
